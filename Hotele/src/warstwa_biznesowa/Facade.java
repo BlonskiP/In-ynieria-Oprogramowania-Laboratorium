9a193c42a49c5cf680290c271f8afe397aba76a7
blonski <piotrblonski96@gmail.com>
2018-12-04 18:24:20 +0100

    lab8 v2

--- Hotele/src/warstwa_biznesowa/Facade.java
+++ Hotele/src/warstwa_biznesowa/Facade.java
@@ -6,6 +6,7 @@
 package warstwa_biznesowa;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -25,6 +26,51 @@ public class Facade {
         client = new Client("test@gmail.com", "haslo", "Jan", "Nowak", "Wroclaw");
         cityList = new ArrayList<City>();
         clientList = new ArrayList<Client>();
+    }
+    public static void main(String[] args)
+    {
+        Facade facade = new Facade();
+        
+        
+        // dodawanie miasta
+        facade.AddCity("Wrocław");
+        for(City city : facade.cityList)
+        {
+            System.out.println(city.name);
+        }
+        
+        // dodawanie hotelu
+        facade.AddHotel("Wrocław", "Hotel 5 Gwiazdkowy", "Plac Grunwaldzki 1");
+        
+        City city = facade.FindCity("Wrocław");
+        for(Hotel hotel : city.hotelList)
+        {
+            System.out.println(hotel.name);
+        }
+        
+        // dodawanie pokoju
+        facade.AddRoom("Wrocław", "Hotel 5 Gwiazdkowy", 2, "Opis", 100);
+        
+        Hotel hotel = city.FindHotel("Hotel 5 Gwiazdkowy");
+        for(Room room : hotel.roomList)
+        {
+            System.out.println("Rozmiar: " + room.size + " | Cena: " + room.price + " | Opis: " + room.description);
+        }
+        
+        // rezerwacja
+        Date date = new Date();
+        
+        // dodanie 3 tygodni ponieważ anulować można najpóxniej 2 tygodnie przed terminem
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.WEEK_OF_MONTH, 3);
+        date = calendar.getTime();
+    
+        facade.MakeReservation("Wrocław", "Hotel 5 Gwiazdkowy", 2, date); // powinien byc success
+        facade.MakeReservation("Wrocław", "Hotel 5 Gwiazdkowy", 2, date); // powinien byc Error
+
+        // anulowanie
+        //facade.CancelReservation("Wrocław", "Hotel 5 Gwiazdkowy", 2, date);
     }
     
     public boolean AddCity(String name)
